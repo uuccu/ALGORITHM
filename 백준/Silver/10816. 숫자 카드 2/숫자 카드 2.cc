3a155208e187a7213fb095cedcd8c689c760d998
TwoCastle9 <37656369+uuccu@users.noreply.github.com>
2024-08-07 23:52:37 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 692 ms, Memory: 25520 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <sstream>
+#include <stack>
+#include <list>
+#include <cmath>
+
+typedef long long ll;
+#define endl "\n"
+using namespace std;
+//
+//
+//long long dp[1001][1001];
+//
+//long long weight_max[1001][1001];
+//long long energy_max[1001][1001];
+//
+//vector<long long> weight;
+//vector<long long> energy;
+//
+//long long recursive(long long l, long long r) {
+//    if (dp[l][r] != -1)
+//        return dp[l][r];
+//
+//    if (l == r) {
+//        weight_max[l][r] = weight[l];
+//        energy_max[l][r] = energy[l];
+//        return dp[l][l] = weight[l] * energy[l];
+//    }
+//
+//    long long min_ans = 2e12;
+//    for (long long i = 1; i <= r - l; i++) {
+//        min_ans = min(min_ans, recursive(l, l + i - 1) + recursive(l + i, r));
+//        weight_max[l][r] = max(weight_max[l][r], max(weight_max[l][l + i - 1], weight_max[l + i][r]));
+//        energy_max[l][r] = max(energy_max[l][r], max(energy_max[l][l + i - 1], energy_max[l + i][r]));
+//    }
+//    long long wm = *max_element(weight.begin() + l, weight.begin() + r + 1);
+//    long long em = *max_element(energy.begin() + l, energy.begin() + r + 1);
+//
+//    weight_max[l][r] = max(weight_max[l][r], wm);
+//    energy_max[l][r] = max(energy_max[l][r], em);
+//
+//    long long once_price = weight_max[l][r] * energy_max[l][r];
+//
+//    return dp[l][r] = min(min_ans, once_price);
+//}
+//
+//void solution() {
+//    long long n;
+//    cin >> n;
+//    weight = vector<long long>(n);
+//    energy = vector<long long>(n);
+//
+//    for (auto &x: weight)cin >> x;
+//    for (auto &x: energy)cin >> x;
+//
+//    memset(dp, -1, sizeof(dp));
+//
+//    cout << recursive(0, n - 1);
+//}
+int n, m;
+vector<int> init_state;
+vector<int> effect[21];
+
+
+void solution() {
+    cin >> n;
+    map<int, int> card;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        card[x]++;
+    }
+
+    cin >> m;
+    for (int i = 0; i < m; i++) {
+        int x;
+        cin >> x;
+        if (card.find(x) != card.end()) {
+            cout << card[x] << " ";
+        } else {
+            cout << 0 << " ";
+        }
+
+    }
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+//    int testcase;
+//    cin >> testcase;
+//    while (testcase--)
+        solution();
+}
+
